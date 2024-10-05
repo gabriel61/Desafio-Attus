1176f1c579c02408458e9e0608575490e2027a10
Gabriel Sampaio <31978199+gabriel61@users.noreply.github.com>
2024-10-05 01:51:29 -0300

    Initial commit

---
+++ src/test/java/com/attus/processojuridico/DesafioAttusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attus.processojuridico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAttusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
